636dfab1b692f546f1cbcf558b769f34032142cf
Jeff Lehnert <jefflehnert@gmail.com>
2013-08-04 17:40:41 +0100

    added a car driving across the screen

--- JSFrogger/scripts/Vehicle.js
+++ JSFrogger/scripts/Vehicle.js
@@ -10,24 +10,17 @@
     Vehicle.prototype.hit = 0;     //average radial disparity
     // constructor:
     Vehicle.prototype.BitmapAnimation_initialize = Vehicle.prototype.initialize; //unique to avoid overiding base class
-
-    // variable members to handle the idle state
-    // and the time to wait before walking again
-    this.isInIdleMode = false;
-    this.idleWaitTicker = 0;
-
+    //75 pixels/sec
+    Vehicle.prototype.speed = 1;
     var quaterFrameSize;
 
     Vehicle.prototype.initialize = function (vehicleName, imgVehicle, x_end) {
         var localSpriteSheet = new createjs.SpriteSheet({
             images: [imgVehicle],
-            frames: { width: 30, height: 30, count: 8,regX: 15,regY:15 },
+            frames: { width: 50, height: 23, count: 1, regX: 25, regY: 11 },
             animations: {
                 // start, end, next, frequency
-                hop: [0, 3, "idle"],
-                hopH: [4,7,"idleH"],
-                idle: 0,
-                idleH: 4
+                driving: [0]
             }
         });
 
@@ -39,76 +32,29 @@
         quaterFrameSize = this.spriteSheet.getFrame(0).rect.width / 4;
 
         // start playing the first sequence:
-        this.gotoAndPlay("idle");     //animate
+        this.gotoAndPlay("driving");     //animate
 
         this.name = vehicleName;
-        // 1 = right & -1 = left and 2 = up and -2 = down
-        this.direction = -1;
+        // 1 = right & -1 = left 
+        this.direction = 1;
         // starting directly at the first frame of the walk_h sequence
         this.currentFrame = 0;
     }
 
     Vehicle.prototype.tick = function () {
-        if (!this.isInIdleMode) {
-            // Moving the sprite based on the direction & the speed
-            var moveDistance = 32;
-            
-            if (this.mustMove == true) {
-                if (this.direction == -2) {
-                    this.y += moveDistance;
-                } else if (this.direction == -1) {
-                    this.x -= moveDistance;
-                } else if (this.direction == 1) {
-                    this.x += moveDistance;
-                } else if (this.direction == 2) {
-                    this.y -= moveDistance;
-                }
-                this.mustMove = false;
-            }
-            // Hit testing the screen width, otherwise our sprite would disappear
-            if (this.x >= this.x_end - (quaterFrameSize + 1) || this.x < (quaterFrameSize + 1)) {
-                this.gotoAndPlay("idle");
-                this.idleWaitTicker = this.IDLEWAITTIME;
-                this.isInIdleMode = true;
-            }
+        // Moving the sprite based on the direction & the speed
+        if (this.direction == -1) {
+            this.x -= this.speed;
+            this.gotoAndPlay("driving");
+        } else if (this.direction == 1) {
+            this.x += this.speed;
+            this.gotoAndPlay("driving_h");
+        } 
+        // Hit testing the screen width, otherwise our sprite would disappear
+        if (this.x >= this.x_end - (quaterFrameSize + 1) || this.x < (quaterFrameSize + 1)) {
+            this.gotoAndPlay("driving");
+            this.idleWaitTicker = this.IDLEWAITTIME;
         }
-        else {
-            this.idleWaitTicker--;
-
-            if (this.idleWaitTicker == 0) {
-                this.isInIdleMode = false;
-
-                // Hit testing the screen width, otherwise our sprite would disappear
-                if (this.x >= this.x_end - (quaterFrameSize + 1)) {
-                    // We've reached the right side of our screen
-                    // We need to walk left now to go back to our initial position
-                    this.direction = -1;
-                    this.gotoAndPlay("hop");
-                }
-
-                if (this.x < (quaterFrameSize + 1)) {
-                    // We've reached the left side of our screen
-                    // We need to walk right now
-                    this.direction = 1;
-                    this.gotoAndPlay("walk_h");
-                }
-            }
-        }
-    }
-
-    Vehicle.prototype.hitPoint = function (tX, tY) {
-        return this.hitRadius(tX, tY, 0);
-    }
-
-    Vehicle.prototype.hitRadius = function (tX, tY, tHit) {
-        //early returns speed it up
-        if (tX - tHit > this.x + this.hit) { return; }
-        if (tX + tHit < this.x - this.hit) { return; }
-        if (tY - tHit > this.y + this.hit) { return; }
-        if (tY + tHit < this.y - this.hit) { return; }
-
-        //now do the circle distance test
-        return this.hit + tHit > Math.sqrt(Math.pow(Math.abs(this.x - tX), 2) + Math.pow(Math.abs(this.y - tY), 2));
     }
 
     window.Vehicle = Vehicle;
